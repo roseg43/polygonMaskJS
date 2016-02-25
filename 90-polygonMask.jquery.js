75258a5f6f847b0fbf2bfc7436f5bb071fcd29a6
Gabe <zealouspencil@gmail.com>
2016-02-25 10:21:41 -0500

    Bugfix and optimizations
    Fixed a bug wherein endless new SVG elements would be appended to the parent items.
    Optimized point recalculation and enclosed the calculation in a function.

--- 90-polygonMask.jquery.js
+++ 90-polygonMask.jquery.js
@@ -12,8 +12,8 @@
 var index = 0;
 var window_width = $(window).outerWidth();
 
-$.fn.polygonMask = function(direction, _recalc_window_width) {
-    
+$.fn.polygonMask = function(direction, _recalc_window_width, options) {
+        
     if (_recalc_window_width) {
         window_width = _recalc_window_width;
     }
@@ -33,10 +33,9 @@ function createMask(el, direction) {
     var $this = el,
       height = $this.outerHeight(),
       offset_top = $this.offset().top;
+      
      
-      
-      console.log($this);
-      
+    
       /**
       	* Firefox automatically attaches SVG clip paths to the top of the parent element while Chrome attaches it to the window.
         * To fix this, we're going to check if the user is using Firefox, and set the top offset to zero if they are.
@@ -52,19 +51,46 @@ function createMask(el, direction) {
      };
      
      var offset = directions[direction];
+     var points_string = calcPoints(direction, offset, offset_top, height);
+     
+      var $currentSVG = $this.find('svg');
+      if ($currentSVG.length > 0) {
+          updatePoints($currentSVG, points_string);
+          return;
+      }
       
     //SVG generation
     var _svgNS = 'http://www.w3.org/2000/svg';
     var svg = "<svg width='0' height='0' xmlns='http://www.w3.org/2000/svg' version='1.1'></svg>";
-
+    
     $this.append(svg);
     var parent = $this.find('svg').get()[0];
 
     var clippath = document.createElementNS(_svgNS, 'clipPath');
     clippath.setAttributeNS(null, 'id', 'clip-' + index);
-    parent.appendChild(clippath);
+
     
-    height += offset_top;
+   
+    console.log('ITEM ' + index + ': ' + points_string);
+
+    var polygon = document.createElementNS(_svgNS, 'polygon');
+    polygon.setAttribute("points", points_string);
+    clippath.innerHTML = polygon.outerHTML;
+    parent.innerHTML = clippath.outerHTML;
+
+		$this.css('-moz-clip-path', 'url(#clip-' + index + ')');
+    $this.css('-webkit-clip-path', 'url(#clip-' + index + ')');
+    $this.css('clip-path', 'url(#clip-' + index + ')');
+    index++;
+}
+
+function updatePoints(svg, points) {
+    var polygon = svg.find('polygon').get()[0];
+    polygon.setAttribute("points", points);
+}
+
+function calcPoints(direction, offset, offset_top, height) {
+     height += offset_top;
     offset += offset_top;
     var points= [];
     if (direction == "up") {
@@ -81,17 +107,7 @@ function createMask(el, direction) {
       points[3] = window_width + " " +  offset;
     }
     
-    var points_string = points.join(',');
-    console.log('ITEM ' + index + ': ' + points_string);
-
-    var polygon = document.createElementNS(_svgNS, 'polygon');
-    polygon.setAttribute("points", points_string);
-    clippath.appendChild(polygon);
-
-		$this.css('-moz-clip-path', 'url(#clip-' + index + ')');
-    $this.css('-webkit-clip-path', 'url(#clip-' + index + ')');
-    $this.css('clip-path', 'url(#clip-' + index + ')');
-    index++;
+    return points.join(',');
 }
 
 $(function() {
